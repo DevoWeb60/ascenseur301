fb56a408c183d2ec0c47ef13f120483f8a32cda5
DevoWeb60 <thibault.berthelin@outlook.fr>
2022-04-26 16:15:28 +0200

    Miniature : update Modal Slider

--- projects/JavaScript/miniatures/app.js
+++ projects/JavaScript/miniatures/app.js
@@ -1,9 +1,12 @@
 const mainImg = document.querySelector(".main img");
+const thumbnailContainer = document.querySelector(".thumbnail");
 const thumbnails = document.querySelectorAll(".thumbnail img");
 const modalButton = document.querySelector("#modal-mode");
 const modal = document.querySelector(".modal");
 const modalClose = modal.querySelector("i");
 const modalImg = modal.querySelector("img");
+const slideLeft = modal.querySelector(".fas.fa-arrow-alt-circle-left");
+const slideRight = modal.querySelector(".fas.fa-arrow-alt-circle-right");
 
 const changeImg = (main, modalImg, newSrc) => {
    if (modalButton.classList.contains("active")) {
@@ -12,6 +15,38 @@ const changeImg = (main, modalImg, newSrc) => {
    } else {
       main.src = newSrc;
    }
+};
+
+const changeSlide = (direction = "right") => {
+   let nextSrc;
+   let previousThumbnail;
+   let nextThumbnail;
+
+   for (let i = 0; i < thumbnails.length; i++) {
+      if (modalImg.src === thumbnails[i].src) {
+         if (direction === "left") {
+            previousThumbnail = thumbnails[i - 1]?.src;
+            previousThumbnail
+               ? (nextSrc = previousThumbnail)
+               : (nextSrc = thumbnails[thumbnails.length - 1].src);
+         } else {
+            nextThumbnail = thumbnails[i + 1]?.src;
+            nextThumbnail
+               ? (nextSrc = nextThumbnail)
+               : (nextSrc = thumbnails[0].src);
+         }
+      }
+   }
+
+   modalImg.src = nextSrc;
+};
+
+const displayImg = (data) => {
+   data.forEach((img) => {
+      const newImg = document.createElement("img");
+      newImg.src = img.download_url;
+      thumbnailContainer.appendChild(newImg);
+   });
 };
 
 thumbnails.forEach((thumbnail) => {
@@ -32,3 +67,6 @@ modalButton.addEventListener("click", () => {
       modalButton.innerText = "Mode popup : Desactivé";
    }
 });
+
+slideLeft.addEventListener("click", () => changeSlide("left"));
+slideRight.addEventListener("click", () => changeSlide());
