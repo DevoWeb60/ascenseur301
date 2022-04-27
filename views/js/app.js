caad2cf117c83f5466aff9f53800c645193c6f59
DevoWeb60 <thibault.berthelin@outlook.fr>
2022-04-27 11:24:46 +0200

    vscode : update new extensions

--- views/js/app.js
+++ views/js/app.js
@@ -8,49 +8,47 @@ const countProject = document.querySelector(".countProject");
 let projectsArray = Array.from(projects);
 
 searchSVG.addEventListener("click", () =>
-    searchSection.classList.toggle("active")
+   searchSection.classList.toggle("active")
 );
 
 searchInput.addEventListener("click", () => {
-    if (window.location.search !== "?search=active") {
-        window.location.search = "?search=active";
-    }
+   if (window.location.search !== "?search=active") {
+      window.location.search = "?search=active";
+   }
 });
 
 function toLowerIncludes(item, value) {
-    item = item.toLowerCase();
-    value = value.toLowerCase();
+   item = item.toLowerCase();
+   value = value.toLowerCase();
 
-    return item.includes(value);
+   return item.includes(value);
 }
 
 if (window.location.search.includes("?search")) {
-    searchInput.focus();
-    searchInput.addEventListener("keyup", () => {
-        projectsContainer.innerHTML = "";
+   searchInput.focus();
+   searchInput.addEventListener("keyup", () => {
+      projectsContainer.innerHTML = "";
 
-        let filteredArray = [];
-        projectsArray.forEach((project) => {
-            let projectTitle = project.children[0];
-            let projectBadgesAndTitle = Array.from(
-                project.children[2].children
-            );
-            projectBadgesAndTitle.push(projectTitle);
+      let filteredArray = [];
+      projectsArray.forEach((project) => {
+         let projectTitle = project.children[0];
+         let projectBadgesAndTitle = Array.from(project.children[2].children);
+         projectBadgesAndTitle.push(projectTitle);
 
-            projectBadgesAndTitle.forEach((content) => {
-                if (toLowerIncludes(content.textContent, searchInput.value)) {
-                    filteredArray.push(project);
-                }
-            });
-        });
+         projectBadgesAndTitle.forEach((content) => {
+            if (toLowerIncludes(content.textContent, searchInput.value)) {
+               filteredArray.push(project);
+            }
+         });
+      });
 
-        filteredArray.forEach((project) => {
-            projectsContainer.append(project);
-        });
-        if (filteredArray.length > 28) {
-            countProject.innerHTML = projectsArray.length;
-        } else {
-            countProject.innerHTML = filteredArray.length;
-        }
-    });
+      filteredArray.forEach((project) => {
+         projectsContainer.append(project);
+      });
+      if (filteredArray.length > 28) {
+         countProject.innerHTML = projectsArray.length;
+      } else {
+         countProject.innerHTML = filteredArray.length;
+      }
+   });
 }
