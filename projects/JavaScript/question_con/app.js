6f23c97c74ec534843f5d06e8193383df9073195
Thibault Berthelin <thibault.berthelin@outlook.fr>
2022-05-10 20:21:02 +0200

    DB-Keyframes: fix responsive Mac

--- projects/JavaScript/question_con/app.js
+++ projects/JavaScript/question_con/app.js
@@ -14,153 +14,153 @@ const displayDatetime = document.querySelector(".datetime");
 let countRestQuestion = [];
 
 class Quizz {
-   constructor(question, answer) {
-      this.question = question;
-      this.answer = answer;
-      this.displayed = false;
-   }
+    constructor(question, answer) {
+        this.question = question;
+        this.answer = answer;
+        this.displayed = false;
+    }
 }
 
 const quizzArray = [
-   new Quizz(
-      "Pourquoi les nameks ont la peau vertes",
-      "Parce que c'est des putains d'escargot"
-   ),
-   new Quizz(
-      "Sur une échelle de A à Z, qu'elle est la couleur préférée des habitants de Gif Sur Yvette",
-      "Chapeau"
-   ),
-   new Quizz("Qu'est-ce que le Mont Blanc", "Un dessert à la vanille"),
-   new Quizz("Pourquoi la vie", "Pourquoi la mort"),
-   new Quizz(
-      "Combien il y a de millisecondes écoulé depuis le 1er Janvier 1970",
-      Date.now()
-   ),
-   new Quizz(
-      "D'autre idée de question",
-      "De toute façons tu peux pas en ajouter"
-   ),
+    new Quizz(
+        "Pourquoi les nameks ont la peau vertes",
+        "Parce que c'est des putains d'escargot"
+    ),
+    new Quizz(
+        "Sur une échelle de A à Z, qu'elle est la couleur préférée des habitants de Gif Sur Yvette",
+        "Chapeau"
+    ),
+    new Quizz("Qu'est-ce que le Mont Blanc", "Un dessert à la vanille"),
+    new Quizz("Pourquoi la vie", "Pourquoi la mort"),
+    new Quizz(
+        "Combien il y a de millisecondes écoulé depuis le 1er Janvier 1970",
+        Date.now()
+    ),
+    new Quizz(
+        "D'autre idée de question",
+        "De toute façons tu peux pas en ajouter"
+    ),
 ];
 
 // FUNCTIONS
 const displayQuestion = () => {
-   let random = Math.round(Math.random() * quizzArray.length - 1);
-   if (random < 0) {
-      random = random + 1;
-   }
+    let random = Math.round(Math.random() * quizzArray.length - 1);
+    if (random < 0) {
+        random = random + 1;
+    }
 
-   countRestQuestion = quizzArray.filter((question) => !question.displayed);
+    countRestQuestion = quizzArray.filter((question) => !question.displayed);
 
-   countQuestionContainer.innerText =
-      countRestQuestion.length + " questions restantes";
+    countQuestionContainer.innerText =
+        countRestQuestion.length + " questions restantes";
 
-   if (countRestQuestion.length === 0) {
-      showAnswerButton.classList.add("disabled");
-      nextQuestionButton.classList.add("disabled");
-      countQuestionContainer.innerText = "";
-      restartButton.classList.add("active");
-      return (questionContainer.innerText =
-         "Désolay je n'ay plu que quaystion !");
-   }
+    if (countRestQuestion.length === 0) {
+        showAnswerButton.classList.add("disabled");
+        nextQuestionButton.classList.add("disabled");
+        countQuestionContainer.innerText = "";
+        restartButton.classList.add("active");
+        return (questionContainer.innerText =
+            "Désolay je n'ay plu que quaystion !");
+    }
 
-   const currentQuizz = quizzArray[random];
-   if (currentQuizz.displayed) {
-      return displayQuestion();
-   } else {
-      currentQuizz.displayed = true;
-      questionContainer.innerText = currentQuizz.question + " ?";
-      answerContainer.innerText = currentQuizz.answer;
-      answerContainer.classList.add("hidden");
-   }
+    const currentQuizz = quizzArray[random];
+    if (currentQuizz.displayed) {
+        return displayQuestion();
+    } else {
+        currentQuizz.displayed = true;
+        questionContainer.innerText = currentQuizz.question + " ?";
+        answerContainer.innerText = currentQuizz.answer;
+        answerContainer.classList.add("hidden");
+    }
 };
 const changeIconForAddQuestion = () => {
-   if (modalContainer.classList.contains("active")) {
-      addQuestionButton.innerHTML = '<i class="fas fa-minus"></i>';
-   } else {
-      addQuestionButton.innerHTML = '<i class="fas fa-plus"></i>';
-   }
+    if (modalContainer.classList.contains("active")) {
+        addQuestionButton.innerHTML = '<i class="fas fa-minus"></i>';
+    } else {
+        addQuestionButton.innerHTML = '<i class="fas fa-plus"></i>';
+    }
 };
 
 const refreshCurrentDatetime = () => {
-   let newDate = new Date();
-   let hour = newDate.getHours();
-   let minutes = newDate.getMinutes();
-   minutes = minutes.toString().padStart(2, "0");
+    let newDate = new Date();
+    let hour = newDate.getHours();
+    let minutes = newDate.getMinutes();
+    minutes = minutes.toString().padStart(2, "0");
 
-   displayDatetime.innerText = hour + ":" + minutes;
+    displayDatetime.innerText = hour + ":" + minutes;
 };
 
 const addQuestion = (question, answer) => {
-   let newQuestion = new Quizz(question, answer);
-   quizzArray.push(newQuestion);
+    let newQuestion = new Quizz(question, answer);
+    quizzArray.push(newQuestion);
 };
 
 const displayError = (element) => {
-   element.placeholder = "Ce champs ne peut pas être vide";
-   element.classList.add("error");
+    element.placeholder = "Ce champs ne peut pas être vide";
+    element.classList.add("error");
 };
 
 // EVENT
 nextQuestionButton.addEventListener("click", () => {
-   displayQuestion();
+    displayQuestion();
 });
 
 showAnswerButton.addEventListener("click", () => {
-   if (!showAnswerButton.classList.contains("disabled")) {
-      answerContainer.classList.toggle("hidden");
-      if (answerContainer.classList.contains("hidden")) {
-         showAnswerButton.innerText = "Voir la réponse";
-      } else {
-         showAnswerButton.innerText = "Cacher la réponse";
-      }
-   }
+    if (!showAnswerButton.classList.contains("disabled")) {
+        answerContainer.classList.toggle("hidden");
+        if (answerContainer.classList.contains("hidden")) {
+            showAnswerButton.innerText = "Voir la réponse";
+        } else {
+            showAnswerButton.innerText = "Cacher la réponse";
+        }
+    }
 });
 
 restartButton.addEventListener("click", () => {
-   quizzArray.forEach((question) => (question.displayed = false));
-   showAnswerButton.classList.remove("disabled");
-   nextQuestionButton.classList.remove("disabled");
-   restartButton.classList.remove("active");
-   displayQuestion();
+    quizzArray.forEach((question) => (question.displayed = false));
+    showAnswerButton.classList.remove("disabled");
+    nextQuestionButton.classList.remove("disabled");
+    restartButton.classList.remove("active");
+    displayQuestion();
 });
 
 addQuestionButton.addEventListener("click", () => {
-   modalContainer.classList.toggle("active");
-   changeIconForAddQuestion();
+    modalContainer.classList.toggle("active");
+    changeIconForAddQuestion();
 });
 
 questionModal.addEventListener("submit", (e) => {
-   e.preventDefault();
-   let question = e.target[0];
-   let answer = e.target[1];
+    e.preventDefault();
+    let question = e.target[0];
+    let answer = e.target[1];
 
-   if (question.value === "") {
-      displayError(question);
-   }
-   if (answer.value === "") {
-      displayError(answer);
-   }
-   if (question.value !== "" && answer.value !== "") {
-      addQuestion(question.value, answer.value);
-      modalContainer.classList.remove("active");
-      countQuestionContainer.innerText =
-         countRestQuestion.length + 1 + " questions restantes";
-      e.target[0].value = "";
-      e.target[1].value = "";
-      changeIconForAddQuestion();
-   }
+    if (question.value === "") {
+        displayError(question);
+    }
+    if (answer.value === "") {
+        displayError(answer);
+    }
+    if (question.value !== "" && answer.value !== "") {
+        addQuestion(question.value, answer.value);
+        modalContainer.classList.remove("active");
+        countQuestionContainer.innerText =
+            countRestQuestion.length + 1 + " questions restantes";
+        e.target[0].value = "";
+        e.target[1].value = "";
+        changeIconForAddQuestion();
+    }
 });
 
 document.addEventListener("click", (e) => {
-   if (e.target === modalContainer) {
-      modalContainer.classList.remove("active");
-      changeIconForAddQuestion();
-   }
+    if (e.target === modalContainer) {
+        modalContainer.classList.remove("active");
+        changeIconForAddQuestion();
+    }
 });
 
 // START
 setInterval(() => {
-   refreshCurrentDatetime();
+    refreshCurrentDatetime();
 }, 10000);
 
 displayQuestion();
